8aa3353f1f616eb567fffa9045fd80983f72fd63
sander.visser@xillio.nl <sanvisser@gmail.com>
2018-07-17 11:21:44 +0200

    init project again

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import {Button, Icon} from 'semantic-ui-react';
+<script src="//cdnjs.cloudflare.com/ajax/libs/annyang/2.6.1/annyang.min.js"/>
 
-const Main = () => (
-    <div className="App">
-        <Button>hoi</Button>
-        <Icon size="massive" loading name='houzz'/>
-    </div>
-);
+export default class Main extends React.Component {
 
-export default Main;
+
+    componentDidMount(){
+        if (annyang) {
+            // Let's define a command.
+            var commands = {
+                'hello': function() { alert('Hello world!'); }
+            };
+
+            // Add our commands to annyang
+            annyang.addCommands(commands);
+
+            // Start listening.
+            annyang.start();
+        }
+    }
+
+    render() {
+        return <div className="App">
+            <Button>hoi</Button>
+            <Icon size="massive" loading name='houzz'/>
+        </div>
+    }
+}
