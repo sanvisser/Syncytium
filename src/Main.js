7d6624f063fde871a69f1a3611759852cc0c9eaa
sander.visser@xillio.nl <sanvisser@gmail.com>
2018-07-17 10:45:06 +0200

    init project

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button, Icon} from 'semantic-ui-react';
+
+const Main = () => (
+    <div className="App">
+        <Button>hoi</Button>
+        <Icon size="massive" loading name='houzz'/>
+    </div>
+);
+
+export default Main;
